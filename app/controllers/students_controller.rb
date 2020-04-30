e0aea513f0311dcc22dd14359bb04dc66388b52b
coralfussman <coralfussman@gmail.com>
2020-04-30 00:01:08 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,19 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
+    
   end
 
   def create
+    Student.create
+    @student = student.create( 
+      {first_name: params[:first_name], 
+      last_name: params[:last_name]})
+
+    redirect_to student_path(@student)
+
+
   end
 
 end
